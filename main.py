67796c41b3edacd0cb8ce0198011f6cdf3b12724
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-28 12:23:07 +0000

    Print to the console using print() and input() functions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def say_hi():
-  print("Hi")
+#using print() function
+print("Hello world!") #Outputs: Hello world!
 
-say_hi()
+#using input() function  
+#Here we are using it with print() function to print it to our console 
+print(input("Input your name \n"))  #Outputs: Hello world!
