89b8be478cfbdb3cade32427aa8762d2102770be
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-28 20:50:03 +0000

    Add print functions with comments

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def say_hi():
-  print("Hi")
+#This is a comment
+print("Hello, World!")
 
-say_hi()
+
+print("Hello, World!")#This is a comment
+
+#This is a comment
+#written in
+#multiple line. 
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
