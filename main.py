b5cb90cb1a4feea9f8cce3c9479a1b5f6e355efb
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-28 12:42:46 +0000

    print integer, float, boolean , strings, float data types to console

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-def say_hi():
-  print("Hi")
+#print an integer
+print(123)
 
-say_hi()
+#print a floating-point number
+print(0.99)
+
+#print a scientific notatation
+print(4e-9)
+print(type(4e-9)) #outputs: <class 'float'>
+
+#print a string 
+print("Hello, my name is Stanley") 
+
+#print a multiline string 
+print(""" 
+###
+hello, this is a multiline string 
+Space is still reserved
+###
+""")
+
+#print a Boolean
+print(True) 
