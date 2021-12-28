6eb1855c04ad047be095dfc2dbe8f196f8bad268
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-28 20:44:25 +0000

    Add another input function

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ print("Hello world!") #Outputs: Hello world!
 
 #using input() function  
 #Here we are using it with print() function to print it to our console 
-print(input("Input your name \n"))  #Outputs: Hello world!
+print(input("Input your name \n"))  
+
+#Save in an identifier and use it inside a print() function 
+a = input("Your last name\n")
+print(a) #print out inputed value
