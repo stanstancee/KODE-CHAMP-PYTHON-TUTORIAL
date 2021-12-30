9f653935e185ab960215697836b0645b1d7fb6d8
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-30 10:08:15 +0000

    Add short cuts

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
-#This is a comment
-print("Hello, World!")
+
+#c = a + b assigns value of a + b into c
+a = 10 
+b= 20
+c = a + b
+print(c) #prints 30
+
+#c += a is equivalent to c = c + a
+c = 10
+a = 4
+c += a
+print(c) #prints 14
+
+#c -= a is equivalent to c = c - a
+a = 5
+c = 10
+c  -= a
+print(c) #prints 5
+
+#c *= a is equivalent to c = c * a
+a = 5
+c = 10
+c  *= a
+print(c) #prints 50
+
+#c /= a is equivalent to c = c / a
+a = 6
+c = 12
+c /= a
+print(c) #prints 2.0
+
+#c %= a is equivalent to c = c % a
+a = 5
+c = 12
+c %= a
+print(c) #prints 2
+
+#c **= a is equivalent to c = c ** a
+a = 2
+c = 4
+c **= a
+print(a) #prints 2
+
+#c //= a is equivalent to c = c // a
+a = 3
+c = 10
+c //=  a
+print(c) #prints 3
 
 
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
