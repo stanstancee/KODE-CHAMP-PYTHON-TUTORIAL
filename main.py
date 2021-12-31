04cbf23251bd276da1b2ddf92c0e10c2a46ad0ba
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2021-12-31 12:03:37 +0000

    add operations

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-#This is a comment
-print("Hello, World!")
 
+ 
+a , b = 12, 6
+# Print bitwise AND operation
+print("a & b =", a & b) #prints a & b = 4
+ 
+# Print bitwise OR operation
+print("a | b =", a | b) #prints a | b = 14
+ 
+# Print bitwise NOT operation
+print("~a =", ~a) #prints ~a = -13
 
-print("Hello, World!")#This is a comment
+# print bitwise XOR operation
+print("a ^ b =", a ^ b) #prints a ^ b = 10
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+a, b = 20, -20 
+# print bitwise right shift operator
+print("a >> 1 =", a >> 1) #prints a >> 1 = 10
+print("b >> 1 =", b >> 1) #prints b >> 1 = -10
+ 
+a, b = 10, -20
+# print bitwise left shift operator
+print("a << 1 =", a << 1) #prints a << 1 = 20
+print("b << 1 =", b << 1) #prints b << 1 = -20
