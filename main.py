e8fca3d90548a8fb4c421244dda40e987d1da955
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-01 13:25:53 +0000

    added boolean operations

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-#This is a comment
-print("Hello, World!")
+# and operator
+print(True and True) #prints True
+print(False and True) #prints False
+print(False and False)#prints False
 
+# or operator
+print(True or True)  #prints True
+print(False or True) #prints True
+print(False or False) #prints False
 
-print("Hello, World!")#This is a comment
+# not operator
+print(not True) #prints True
+print(not False) #prints True
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+#numeric boolean value
+print(1 and 1) #prints 1
+print(0 and 1) #prints 0
+print(0 and 0) #prints 0
 
+#numeric boolean value 
+print(1 or 1) #prints 1
+print(0 or 1) #prints 1
+print(0 or 0) #prints 0
 
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+print(True + True) #prints 2
+print(False + False) #prints 0
