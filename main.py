b4fc748b794c92d2c783800b210e2dd9bb878482
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-01 13:54:53 +0000

    add boolean expressions

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-#This is a comment
-print("Hello, World!")
-
-
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+x = 10
+y = 20
+#Equal operator
+print(x == y)  #prints False
+#Not equal operator
+print(x != y) #prints True
+#Greater than
+print(x > y) #prints False
+#Less than
+print(x < y) #prints True
+#Greater than or equal to
+print(x >= y) #prints False
+#Less than or equal to
+print(x <= y) #prints True
