a0ed224043707fb92410e9bd453afc30679ec508
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-03 10:59:28 +0000

    add complex expression

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#This is a comment
-print("Hello, World!")
+age_x = 20
+age_y = 15
+height_x = 5
+height_y = 6
 
+print((age_x > age_y) and (height_x > height_y)) # prints False
 
-print("Hello, World!")#This is a comment
+print((age_x < age_y) or (height_x < height_y)) # prints True
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+print((age_x < age_y) and (height_x > height_y)) # prints False
 
+print((age_x < age_y) or (height_x > height_y)) # prints False
 
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+print(not (age_y > age_x)) #True
+
+print(not (age_x > age_y) and (height_x > height_y)) #False
+
+print((not (age_x > age_y) and (height_x > height_y)) or (age_x > age_y)) #True
