3143a92e7615b9408aa23eb34404242c3b490191
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-03 11:58:16 +0000

    Add floats

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-#This is a comment
-print("Hello, World!")
-
-
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+#floats 
+print(0.1 + 0.2) #prints 0.3000000000000004
+print(0.5) #prints 0.5
+print(1/2) #prints 0.5
+print( 0.0000000002) #prints 2e-10
