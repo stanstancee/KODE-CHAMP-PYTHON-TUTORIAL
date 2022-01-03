ab03e752f16a472f4c64578f2b7f209fc329cc56
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-03 14:21:42 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-#This is a comment
-print("Hello, World!")
+#str
+x = 24
+y = 0.5
+string_x = str(x) 
+string_y = str(y)
+print(string_x, string_y, sep=" + ") #outputs: 24 + 0.5
+print(string_x  + string_y) #outputs: 240.5
 
+#int
+x = "50"
+y = 8.9
+integer_x = int(x)
+integer_y = int(y)
+print(integer_y) #prints 8
+print(integer_y + integer_x) #prints 58
 
-print("Hello, World!")#This is a comment
+#floats conversion
+x = "34"
+y = 20
+x = float(x) 
+y = float(y)
+print(x)       #prints 34.0
+print(x - y)   #prints 14.0
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+#len
+#len() cannot parse integers and floats, convert to strings before parsing
+x , y , z = 1000, 33.89, "This is a string"
+x , y = str(x) , str(y)
+x,y,z = len(x), len(y) , len(z)
+print(x) #outputs 4
+print(y) #outputs 5
+print(z) #outputs 16 : All character counted, including the spaces
 
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
