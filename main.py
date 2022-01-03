5d76a6b4133f698e0f14dfe36156fe842a382a80
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-03 16:14:23 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #This is a comment
-print("Hello, World!")
+#python will convert integer (whole number) to float to avoid data loss
+x = 23.7
+y = 19
+sum = x + y # 23.7 + 19.0
+print(sum) # 42.7
 
+#python will convert any bool to an int using arithmetic ooperators on both
+x = True
+y = 1
+sum = x + y # 1 + 4
+print(sum) #prints 5
 
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+#python will not be implicitly convert in some cases like:
+#suming int and string, or boolean and string
+print("Hello" + 1) #raise type error
+print(True + "False") # Raise type error
