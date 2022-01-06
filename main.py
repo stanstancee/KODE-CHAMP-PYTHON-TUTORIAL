56b1dbf667641e14412fe013233919838c445387
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-06 14:39:03 +0000

    add range examples

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-#This is a comment
-print("Hello, World!")
+#iterating range with for loop
+for i in range(6):
+  print(i) #print 0 to 5
+ 
+ #explicitly convert range to list
+my_range = range(5)
+print(list(my_range)) # [0,1,2,3,4]
+
+#start and stop arguments
+for i in range(4,10):
+  print(i) #print 4 to 9
+
+#step argument
+#print all even numbers from 2 to 10
+for i in range(2,10,2):
+  print(str(i) + " is even")
 
 
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
 
 
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+
