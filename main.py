d758e4cb21801a870cd75110b3aced5a7540f9ba
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-06 13:17:16 +0000

    Added for loop

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-#This is a comment
-print("Hello, World!")
+my_string = "Success"
+#print all characters in capital letter 
+for char in my_string:
+  print(char.upper())
 
 
-print("Hello, World!")#This is a comment
+#iterating over list
+numbers = [2,5,6]
+#multiply each number in the list by 2
+for number in  numbers:
+  print(number * 2)
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+#for/else loop
+digits_in_string = "123456"
+for digit in digits_in_string:
+  if int(digit) % 2 == 0:
+    print(digit + " is " + "even")
+   
+else:
+  print("Finished getting even numbers.")
 
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
