3077eb7e7f111e6726b13fa81826b6fd58f6adc6
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-06 15:15:11 +0000

    Add break statement

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-#This is a comment
-print("Hello, World!")
+#break inside simple while loop
+x = 1
+while x <= 10:
+  print(x)
+  if(x == 5):
+    print("We have found 5. Goodbye!")
+    break
+
+  x += 1
+
+i = 0
+loading = True
+while loading:
+  i = i + 1
+  if(i == 10):
+    print("breaking....")
+    break
+  print("Loading....")
+print("Goodbye!")
 
 
-print("Hello, World!")#This is a comment
+#break inside for loop
+for val in "Stanley":
+    if val == "l":
+        break
+    print(val)
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+print("The end")
 
+#break inside for loop
+one_to_twenty = range(34)
+for val in one_to_twenty :
+  mid_val = list(one_to_twenty)
+  mid_val = len(mid_val)// 2
+  if(val == mid_val):
+    print(val,"breaking... mid-value reached")
+    break
+  print(val)
+  
 
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+  
