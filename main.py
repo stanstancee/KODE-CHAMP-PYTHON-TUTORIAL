f07490804f08ab8404c22c2da9b7610ae6e9d641
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-09 19:46:50 +0000

    Add tuple

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-#This is a comment
-print("Hello, World!")
-
-
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+#creating tuples with parenthesis
+cord = (12.4,23.5)
+print(cord) # (12.4, 23.5)
+# creating tuple without parenthesis
+dimensions = 34, 55, 45, 23, 9, 90, 43
+print(dimensions) # (34, 55, 45, 23, 9, 90, 43)
+#indexing
+first_value = dimensions[0]
+last_value = dimensions[-1]
+fifth_value = dimensions[4]
+print(first_value, last_value, fifth_value , sep="\n")
