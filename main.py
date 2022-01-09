6c9584af7687f0d5a77b3fffd25d8ce50314882d
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-09 20:44:26 +0000

    Add slicing

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#This is a comment
-print("Hello, World!")
+# slicing
+cities = ("Ikeja", "Asaba", "Benin" , "Ibadan", "Lokoja" , "Jos")
+# from index 3 to index 5 (5 not included)
+three_to_five = cities[3:5]
+print(three_to_five ) # ('Ibadan', 'Lokoja')
+# from 2 till the end
+two_till_end = cities[2:]
+print(two_till_end) # ( 'Benin' , 'Ibadan', 'Lokoja' , 'Jos')
+#from start to 5
+start_to_five =  cities[:5]
+print(start_to_five) # ('Ikeja', 'Asaba', 'Benin' , 'Ibadan', 'Lokoja' )
 
-
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+# tuple unpacking
+dimensions = 20 , 6 , 50
+length, width , height = dimensions
+product = length * width * height 
+print(length) # 20
+print(height) # 50
+print(product) #6000
+#imutability
+dimensions[0] = 40 #TypeError
+print(dimensions)
