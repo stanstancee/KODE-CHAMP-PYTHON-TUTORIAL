6265635a4ea9267a44e7cab42614677aab21b25e
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-10 11:44:08 +0000

    Change list to tuple

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ week = {1: 'sunday', 2: 'monday', 3: 'tuesday', 4: 'wednesday', 5: 'thursday', 6
 for day in week:
   print(day)
 
-abbr = ["st", "nd", "rd", "th"]
+abbr = ("st", "nd", "rd", "th")
 for day in week:
   day_abbr = str(day)+ abbr[(day-1)] if day < 4 else str(day)+ abbr[-1]
   print(day_abbr , " day is " , week[day] )
