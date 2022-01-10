6fb2e3b93c56957537097553f97005c8bd4ab668
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-10 12:22:04 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-#This is a comment
-print("Hello, World!")
+# keys() method
+week = {1: 'sunday', 2: 'monday', 3: 'tuesday', 4: 'wednesday', 5: 'thursday', 6: 'friday', 7: 'saturday'}
+
+#create a key view
+keys = week.keys()
+print(keys)
+abbr = ["st", "nd" , "rd", "th"]
+for key in keys:
+  values = week[key]
+  abbr_key = str(key) + abbr[(key - 1)] if key < 4 else str(key) + abbr[-1]
+  print( abbr_key , " day is " , values )
+
+# values() method
+values = week.values()
+print(values)
+for value  in values:
+  print(value.upper())
+
+#membership test
+print("monday" in week.values())
+print(4 in  week.keys())
+
+#items() method
+week = week.items()
+print(week)
+for key , value in week:
+  print( key , " --- ", value)
 
 
-print("Hello, World!")#This is a comment
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+# using while
+# x = True
+# while x :
+  
+#   read_line = input('Enter days of the week e.g 2 or Monday', "Enter end to stop")
+#   if read_line == "end".lower():
+#     x = False
+  
 
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
