cdacc02bf41fb45da2533e8e155d9755450a4e7f
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-10 10:55:03 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-#This is a comment
-print("Hello, World!")
+# dictionaries
+# empty dictionary
+elements = {}
+week = {1:"sunday", 2:"monday", 3:"tuesday", 4:"wednesday", 5:"thursday", 6:"friday", 7:"saturday"}
+print(week)
 
+elements["hydrogen"] = 1
+elements["helium"] = 2
+elements["carbon"] = 6
+print(elements)
 
-print("Hello, World!")#This is a comment
-
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
-
-
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+#indexing
+helium = elements["helium"]
+second_day = week[2]
+print(second_day) # monday
+print(helium) # 2
