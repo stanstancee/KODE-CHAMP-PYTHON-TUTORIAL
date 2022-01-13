24aeee863be63abd5dd5e2a8b2cd130b882b9837
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-13 19:06:08 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,75 +1,17 @@
-# infinite loop with  python generators
-def all_even():
-    n = 0
-    while True:
-        yield n
-        n += 2
+#generator short-hand
+my_odds_gn = (x for x in range(3000) if not x % 2 == 0)
+my_odds_lt = [x for x in range(3000) if not x % 2 == 0]
+print(my_odds_gn) # <generator object <genexpr> at 0x7f2ae827c2e0>
+print(my_odds_lt) # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
-# for i in all_even():
-#   print(i)
+# convert generator to a list
+odds = list(my_odds_gn)
+print(odds) # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+# profiling
+import sys
+print(sys.getsizeof(my_odds_gn)) # 112 bytes
+print(sys.getsizeof(my_odds_lt)) # 13000 bytes
 
-#pipeline generators
-def fibonacci_numbers(nums):
-    x, y = 0, 1
-    for _ in range(nums):
-        x, y = y, x+y
-        yield x
-    
-
-def square(nums):
-    for num in nums:
-        yield num**2
-
-
-fib = fibonacci_numbers(10)
-my_square = square(fib)
-print(my_square)
-for i in my_square:
-  print(i)
-
-#factorial
-# fn = fn - 1
-def fac(num):
-  add = 1
-  for i in range(1,num + 1):
-    add = add * i
-    yield add
-
-x = 1
-for i in fac(10):
-  print( "factorial of ", x  ," is ", i)
-  x += 1
-    
-def is_palindrome(num):
-    # Skip single-digit inputs
-    if num // 10 == 0:
-        return False
-    temp = num
-    reversed_num = 0
-
-    while temp != 0:
-        reversed_num = (reversed_num * 10) + (temp % 10)
-        temp = temp // 10
-
-    if num == reversed_num:
-        return num
-    else:
-        return False
-
-
-def infinite_sequence():
-  num  = 0
-  while True:
-    yield num
-    num += 1
-
-for i in infinite_sequence():
-  pal = is_palindrome(i)
-  if pal:
-    print(i)
-
-
-
-    
-
-
+import cProfile
+cProfile.run('sum((x for x in range(3000) if not x % 2 == 0))') # 0.007 seconds
+cProfile.run('sum([x for x in range(3000) if not x % 2 == 0])') # 0.001 seconds
