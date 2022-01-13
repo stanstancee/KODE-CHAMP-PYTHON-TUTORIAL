91b92b17b9dd4182b8a3e3b571ef304719c08729
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-13 20:16:16 +0000

    Add examples

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-#This is a comment
-print("Hello, World!")
+# function args positional argument
+def my_average(*args):
+  my_len = len(args)
+  average = sum(args) // my_len
+  return average
+  
 
+print(my_average(23, 45, 56)) # 41
 
-print("Hello, World!")#This is a comment
+def area(*args):
+  base , height = args
+  calc = 1/2 * (base * height)
+  print("Area of the triangle = {}".format(calc))
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+area(29,4)
 
+def force(**kwargs):
+  # unpacking variables
+  m, v, u, t = kwargs.values()
+  a = (v - u)/t
+  f = m * a
+  return "force = {}N".format(f)
 
 """
-This is a comment
-written in
-more than just one line
+Mass of the body, m = 3 kg
+Initial speed of body, u = 2 m/s
+Final speed of body, v = 3.5 m/s
+Time, t = 25 s
 """
-print("Hello, World!")
+force = force(m = 3, v = 3.5, u = 2, t = 25)
+print(force)
