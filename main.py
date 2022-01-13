244924ea0466abe55c839d271369b26d3187f7b5
Ifeoha Chekwube Stanley <stanstancee@gmail.com>
2022-01-13 18:08:35 +0000

    Add generators

--- main.py
+++ main.py
@@ -1,18 +1,76 @@
-#This is a comment
-print("Hello, World!")
+# infinite loop with  python generators
+def all_even():
+    n = 0
+    while True:
+        yield n
+        n += 2
+
+# for i in all_even():
+#   print(i)
+
+#pipeline generators
+def fibonacci_numbers(nums):
+    x, y = 0, 1
+    for _ in range(nums):
+        x, y = y, x+y
+        yield x
+    
+
+def square(nums):
+    for num in nums:
+        yield num**2
 
 
-print("Hello, World!")#This is a comment
+fib = fibonacci_numbers(10)
+my_square = square(fib)
+print(my_square)
+for i in my_square:
+  print(i)
 
-#This is a comment
-#written in
-#multiple line. 
-print("Hello, World!")
+#factorial
+# fn = fn - 1
+def fac(num):
+  add = 1
+  for i in range(1,num + 1):
+    add = add * i
+    yield add
+
+x = 1
+for i in fac(10):
+  print( "factorial of ", x  ," is ", i)
+  x += 1
+    
+def is_palindrome(num):
+    # Skip single-digit inputs
+    if num // 10 == 0:
+        return False
+    temp = num
+    reversed_num = 0
+
+    while temp != 0:
+        reversed_num = (reversed_num * 10) + (temp % 10)
+        temp = temp // 10
+
+    if num == reversed_num:
+        return num
+    else:
+        return False
 
 
-"""
-This is a comment
-written in
-more than just one line
-"""
-print("Hello, World!")
+def infinite_sequence():
+  num  = 0
+  while True:
+    yield num
+    num += 1
+
+for i in infinite_sequence():
+  pal = is_palindrome(i)
+  if pal:
+    print(i)
+
+
+
+
+    
+
+
